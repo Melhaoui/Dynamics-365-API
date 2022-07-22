522476db8a0d8333e308ecaa631b9b7f556d6cc9
Abdessamad Brs <abdsamad.brs@gmail.com>
2022-07-22 18:36:13 +0100

    Opportunitie

--- Dynamics365API/Controllers/CrmController.cs
+++ Dynamics365API/Controllers/CrmController.cs
@@ -1,6 +1,8 @@
-﻿using Dynamics365API.Services;
+﻿using Dynamics365API.Dtos;
+using Dynamics365API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Dynamics365API.Controllers
 {
@@ -9,18 +11,42 @@ namespace Dynamics365API.Controllers
     public class CrmController : ControllerBase
     {
         private readonly ICrmService _crmService;
-
-        public CrmController(ICrmService crmService)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CrmController(ICrmService crmService, IHttpContextAccessor httpContextAccessor)
         {
             _crmService = crmService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         [HttpGet("contacts")]
         public async Task<IActionResult> ContactsAsync()
         {
             var result = await _crmService.GetEntity("contacts");
-
+           
             return Ok(result);
         }
+
+        [HttpGet("opportunities")]
+        public async Task<IActionResult> OpportunitiesAsync()
+        {
+            var user = _httpContextAccessor.HttpContext?.User;
+            var email = user.FindFirst(ClaimTypes.Email).Value;
+
+            var result = await _crmService.GetEntity($"opportunities?$select=name,emailaddress,totalamount,actualclosedate,estimatedclosedate,actualvalue,closeprobability&$filter=emailaddress eq '{email}'");
+            
+            
+            //emailaddress  name    totalamount( Revenu estimé)  actualclosedate    estimatedclosedate   actualvalue(Revenu réel)  closeprobability
+            //classement -interes  -tree inter  -peut intrs
+            return Ok(result);
+        }
+
+        [HttpPost("AddAccount")]
+        public async Task<IActionResult> AddAccountAsync([FromBody] object obj)
+        {
+            var result = await _crmService.AddEntity("opportunities?$select=name", obj);
+            //?$filter=contains(emailaddress,'miguel@northwindtraders.com')
+            return Ok(result);
+        }
+
     }
 }
--- Dynamics365API/Dtos/CrmCheckEmailDataDto.cs
+++ Dynamics365API/Dtos/CrmCheckEmailDataDto.cs
@@ -5,14 +5,14 @@ namespace Dynamics365API.Dtos
     public class CrmCheckEmailDataDto
     {
         [Display(Name = "@odata.context")]
-        public string? odataContext { get; set; }
+        public string odataContext { get; set; }
 
         [Display(Name = "@Microsoft.Dynamics.CRM.totalrecordcount")]
-        public int crmTotalrecordcount { get; set; }
+        public int? crmTotalrecordcount { get; set; }
 
 
         [Display(Name = "@Microsoft.Dynamics.CRM.totalrecordcountlimitexceeded")]
-        public bool crmtotalrecordcountlimitexceeded { get; set; }
+        public bool? crmtotalrecordcountlimitexceeded { get; set; }
 
         [Display(Name = "Microsoft.Dynamics.CRM.globalmetadataversion")]
         public string? crmGlobalmetadataV { get; set; }
