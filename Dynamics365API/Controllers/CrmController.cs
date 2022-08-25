c7b52bffe8eb977a792041605c1092afd3ed98e0
Melhaoui <melhaouimed.96@gmail.com>
2022-08-25 11:24:20 +0100

    Add contact details and add statuscode to Opportunities

--- Dynamics365API/Controllers/CrmController.cs
+++ Dynamics365API/Controllers/CrmController.cs
@@ -1,4 +1,5 @@
 ﻿using Dynamics365API.Dtos;
+using Dynamics365API.Dtos.Crm;
 using Dynamics365API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -38,7 +39,7 @@ namespace Dynamics365API.Controllers
             var user = _httpContextAccessor.HttpContext?.User;
             var email = user.FindFirst(ClaimTypes.Email).Value;
 
-            var result = await _crmService.GetEntityAsync($"opportunities?$select=name,emailaddress,totalamount,actualclosedate,estimatedclosedate,actualvalue,closeprobability&$filter=emailaddress eq '{email}'");
+            var result = await _crmService.GetEntityAsync($"opportunities?$select=name,emailaddress,totalamount,actualclosedate,estimatedclosedate,actualvalue,closeprobability, statuscode&$filter=emailaddress eq '{email}'");
             
             
             //emailaddress  name    totalamount( Revenu estimé)  actualclosedate    estimatedclosedate   actualvalue(Revenu réel)  closeprobability
@@ -52,7 +53,7 @@ namespace Dynamics365API.Controllers
             var result = (dynamic)null;
             var user = await _authService.GetCurrentUserAsync(_httpContextAccessor);
             var allEmailTeamNotPrimaryFilter = await _crmService.GetAllEmailTeamAsync(user.Email, "emailaddress");
-            result = await _crmService.GetEntityAsync($"opportunities?$select=name,emailaddress,totalamount,actualclosedate,estimatedclosedate,actualvalue,closeprobability&$filter={allEmailTeamNotPrimaryFilter} ");
+            result = await _crmService.GetEntityAsync($"opportunities?$select=name,emailaddress,totalamount,actualclosedate,estimatedclosedate,actualvalue,closeprobability, statuscode&$filter={allEmailTeamNotPrimaryFilter} ");
 
             return Ok(result);
         }
@@ -64,6 +65,14 @@ namespace Dynamics365API.Controllers
             var user = await _authService.GetCurrentUserAsync(_httpContextAccessor);
             var allEmailTeamNotPrimaryFilter = await _crmService.GetAllEmailTeamAsync(user.Email, "emailaddress1");
             result = await _crmService.GetEntityAsync($"contacts?$select=fullname, emailaddress1, telephone1&$filter={allEmailTeamNotPrimaryFilter} ");
+
+            return Ok(result);
+        }
+
+        [HttpGet("contactsDetails")]
+        public async Task<IActionResult> ProfileAsync([FromBody] CrmContactDetails crmContactDetails)
+        {
+            var result = await _crmService.GetEntityAsync($"contacts?$select=contactid, firstname,lastname, jobtitle, emailaddress1, telephone1, mobilephone, fax, preferredcontactmethodcode, address1_line1, address1_line2, address1_line3, address1_city, address1_stateorprovince, address1_postalcode, address1_country, entityimage_url, gendercode, familystatuscode, spousesname, birthdate, anniversary&$expand=parentcustomerid_account($select=name)&$filter=emailaddress1 eq '{crmContactDetails.Email}'");
 
             return Ok(result);
         }
@@ -109,14 +118,5 @@ namespace Dynamics365API.Controllers
 
             return Ok(result);
         }
-
-        [HttpPost("AddAccount")]
-        public async Task<IActionResult> AddAccountAsync([FromBody] object obj)
-        {
-            var result = await _crmService.AddEntityAsync("opportunities?$select=name", obj);
-            //?$filter=contains(emailaddress,'miguel@northwindtraders.com')
-            return Ok(result);
-        }
-
     }
 }
